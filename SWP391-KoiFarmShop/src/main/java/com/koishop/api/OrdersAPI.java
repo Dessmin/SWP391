0a2658c9cd01dd50f2557fbc68b6d56508c4300e
H4aiDang <dangnthse170366@fpt.edu.vn>
2024-10-11 21:38:22 +0700

    Update order

--- SWP391-KoiFarmShop/src/main/java/com/koishop/api/OrdersAPI.java
+++ SWP391-KoiFarmShop/src/main/java/com/koishop/api/OrdersAPI.java
@@ -2,6 +2,7 @@ package com.koishop.api;
 
 import com.koishop.entity.Orders;
 import com.koishop.models.orders_model.OrdersRequest;
+import com.koishop.models.orders_model.OrdersResponse;
 import com.koishop.service.OrdersService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,26 +20,38 @@ public class OrdersAPI {
     @Autowired
     private OrdersService ordersService;
 
-    @GetMapping("list-order")
+    @GetMapping("{OrderId}/get-order")
+    public ResponseEntity getOrderById(Integer id) {
+        OrdersResponse orders = ordersService.getOderById(id);
+        return  ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("list-all-orders")
+    public ResponseEntity getAllOrders() {
+        List<OrdersResponse> orders = ordersService.getAllOrders();
+        return  ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("list-user-orders")
     public ResponseEntity getAllOrdersByUser() {
-        List<Orders> orders = ordersService.getAllOrdersByUser();
+        List<OrdersResponse> orders = ordersService.getAllOrdersByUser();
         return ResponseEntity.ok(orders);
     }
 
 
     @PostMapping("add-order")
     public ResponseEntity createOrder(@RequestBody OrdersRequest ordersRequest) {
-        Orders createdOrder = ordersService.createOrder(ordersRequest);
+        OrdersResponse createdOrder = ordersService.createOrder(ordersRequest);
         return ResponseEntity.ok(createdOrder);
     }
 
-    @PutMapping("/{orderID}/update")
+    @PutMapping("/{orderId}/update")
     public ResponseEntity updateOrder(@PathVariable int orderID, @RequestBody OrdersRequest ordersRequest) {
-        Orders updatedOrder = ordersService.updateOrder(orderID, ordersRequest);
+        OrdersResponse updatedOrder = ordersService.updateOrder(orderID, ordersRequest);
         return ResponseEntity.ok(updatedOrder);
     }
 
-    @DeleteMapping("/{orderID}/remove")
+    @DeleteMapping("/{orderId}/remove")
     public ResponseEntity<Void> deleteOrder(@PathVariable int orderID) {
         ordersService.deleteOrder(orderID);
         return ResponseEntity.noContent().build();
