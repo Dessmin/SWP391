9c7ac6eb12b2eca73b5ba2f2fea5fa4793cfa515
Dessmin <quanglong281381@gmail.com>
2024-11-11 05:45:32 +0700

    Delete SWP391-KoiFarmShop folder

--- SWP391-KoiFarmShop/src/main/java/com/koishop/repository/ConsignmentRequestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.koishop.repository;
-
-import com.koishop.entity.ConsignmentRequest;
-import com.koishop.entity.KoiFish;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConsignmentRequestRepository extends JpaRepository<ConsignmentRequest, Integer> {
-}
